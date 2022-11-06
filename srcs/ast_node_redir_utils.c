e7792acbbb0f74317b337f5797c711326a1f2550
Florian BILY <fbily@e2r7p20.clusters.42paris.fr>
2022-11-06 20:44:51 +0100

    Suite built in, env ok, unset segfault

--- srcs/ast_node_redir_utils.c
+++ srcs/ast_node_redir_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ast_node_redir_utils.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbeylot <sbeylot@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fbily <fbily@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 07:43:20 by sbeylot           #+#    #+#             */
-/*   Updated: 2022/11/04 12:56:24 by sbeylot          ###   ########.fr       */
+/*   Updated: 2022/11/06 18:20:22 by fbily            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 t_node	*init_redir(t_token **token)
 {
-   t_node	*node;
+	t_node	*node;
 
-   node = (t_node *)malloc(sizeof(t_node));
-   if (!node)
-	   return (NULL);
-   init_node_redir(&node);
-   if (get_ttype(token) == IONUMBER)
-	   assign_ionumber(&node, token);
-   if (valid_redir(*token))
-	   assign_redir(&node, token);
-   else
-	   return (free(node), NULL);
-   return (node);
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	init_node_redir(&node);
+	if (get_ttype(token) == IONUMBER)
+		assign_ionumber(&node, token);
+	if (valid_redir(*token))
+		assign_redir(&node, token);
+	else
+		return (free(node), NULL);
+	return (node);
 }
 
 void	init_node_redir(t_node **node)
